610cc6ad9c3902c96c79c23314d21d958b3358f7
arathyac <ubuntu@ip-172-31-31-230.ec2.internal>
2026-06-16 06:27:01 +0000

    Fix part 2 dashboard template and static files

--- static/script.js
+++ static/script.js
@@ -1,10 +1,9 @@
-
 function randomValue(min, max) {
     return (Math.random() * (max - min) + min).toFixed(1);
 }
 
 function updateDashboard() {
-    document.getElementById("cpu").innerText = randomValue(0, 100) + "%";
+    document.getElementById("cpu").innerText = randomValue(1, 80) + "%";
     document.getElementById("memory").innerText = randomValue(20, 90) + "%";
     document.getElementById("disk").innerText = randomValue(30, 95) + "%";
 
